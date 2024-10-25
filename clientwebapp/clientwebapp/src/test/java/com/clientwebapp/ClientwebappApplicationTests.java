8607b24f9fa63751be59bf0a78cda72465f997ef
shalinipaidimuddala <shalinipaidimuddala@gmail.com>
2024-10-25 15:27:55 +0530

    Add files via upload

---
+++ clientwebapp/clientwebapp/src/test/java/com/clientwebapp/ClientwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eurekhaserver/src/main/java/com/eurekhaserver/EurekhaserverApplication.java
@@ -0,0 +1,13 @@
+package com.eurekhaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekhaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekhaserverApplication.class, args);
+	}
+
+}
